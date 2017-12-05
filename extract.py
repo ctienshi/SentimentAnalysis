4067ef6de3ac217bf23d80addc3c742cb85bab99
ctienshi <ctienshi@gmail.com>
2017-12-05 13:20:13 +0530

    adding extract.py

---
+++ extract.py
@@ -0,0 +1,32 @@
+from Sentiment import calEmotionalLevel
+from Sentiment import tes
+from openpyxl import load_workbook
+wb=load_workbook("/home/ching/Downloads/bizdev.xlsx")
+
+ws = wb.active
+first_column = ws['E']
+col = ws['L']
+test = str(first_column[4].value)
+
+word = "You received this message because"
+
+for i in range(50,70):
+    a = str(first_column[i].value)
+    a = a[:a.find(word)]
+    #print (a)
+    #print ('\n')
+
+    x = calEmotionalLevel(a)
+    p = tes(a)
+    column_cell = 'L'
+    cell = 'M'
+
+    ws[column_cell+str(i+1)] = x
+    ws[cell+str(i+1)] = p
+
+    print(str(i+1)+" " +str(x)) + " " + str(p)
+    print ("\n")
+
+
+wb.save("/home/ching/Downloads/bizdev.xlsx")
+
